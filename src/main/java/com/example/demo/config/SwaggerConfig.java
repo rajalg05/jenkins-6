01736ad3325b66344d51e16c44c7e083696d87e1
Lalatendu Guru <rajalg05@gmail.com>
2021-01-21 11:36:39 +0530

    first commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket postsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Jenkins").apiInfo(apiInfo()).select()
+				.paths(regex("/test.*")).build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Jenkins git docker")
+				.description("Jenkins git docker")
+				.build();
+	}
+}
---
+++ src/main/java/com/example/demo/controller/TrimsController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TrimsController {
+
+	@GetMapping("/api")
+	public String trimsApi() {
+		return "1 - Test successfull!!";
+	}
+}
